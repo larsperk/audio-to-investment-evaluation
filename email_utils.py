cd746987c894297d42c8232f2a5a0e89471ef1bb
larsperk <lars@larsperkins.com>
2023-11-18 17:33:58 +0000

    add support for email body summarization - fix 2

--- email_utils.py
+++ email_utils.py
@@ -252,8 +252,7 @@ def get_emails_and_create_work_files():
                             if part.get_content_maintype() == 'multipart':
                                 continue
                             if part.get('Content-Disposition') is None:
-                                msg_txt = msg_txt or part.get_payload(decode=True).decode()
-                                continue
+                                msg_txt = msg_txt or part.get_payload()
 
                             filename = part.get_filename().upper()
                             filepath = ""
